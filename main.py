dae56ce7aae6d3a74cf5f857b568e57f90485002
VH <hirawatt@pm.me>
2023-01-02 17:54:20 +0530

    added requirements.txt

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from annotated_text import annotated_text
-
 raw_data = pd.read_csv('all-companies-list.csv', dtype={'BSE Code': str})
+
+# Combination Sum
+from itertools import combinations
+
+@st.cache
+def find_combos(arr, no_of_stocks):
+    combos = list(combinations(arr, no_of_stocks))
+    combo_sums = []
+    for combo in combos:
+        combo_sums.append(sum(combo))
+    return combo_sums
 
 def main():
     st.sidebar.subheader("Fund Stock Screener")
@@ -21,13 +30,11 @@ def main():
 
     avg_investment_value = round(min_investment/no_of_stocks, 2)
     max_stock_price = round(max_portfolio_allocation*min_investment/100, 2)
-    annotated_text(
-        (str(avg_investment_value), "Average Investment Value"),
-        (str(max_stock_price), "Maximum Stock Price")
-    )
+    c1, c2 = st.columns(2)
+    c1.metric("Average Investment Value", avg_investment_value)
+    c2.metric("Max. Stock Price", max_stock_price)
 
     # Stock Search Base Dataframe
-    st.subheader("Stock Search")
     df = raw_data.iloc[:, [0, 1, 2, 3, 4]]
 
     ## Small-Cap Filter
@@ -42,19 +49,40 @@ def main():
     nse_only_list = small_cap["BSE Code"].isnull()
     nse_only = small_cap[nse_only_list]
 
-    st.write(small_cap)
-    annotated_text(
-        "❄️",
-        (str(sum(bse_only_list) + sum(nse_only_list)), "Only NSE & Only BSE"),
-        (str(nse_bse_small_cap[0]), "Both NSE & BSE"),
-        "❄️",
-        (str(sum(bse_only_list) + sum(nse_only_list) + nse_bse_small_cap[0]), "Sum"),
-        "❄️",
-        (str(small_cap.count()[3]), "All Small Cap")
-    )
+    a1, a2, a3, a4 = st.columns(4)
+    a1.metric("No. of Small Cap Companies", str(small_cap.count()[3]))
+    a2.metric("No. of BSE Only Companies", str(bse_only.count()[3]))
+    a3.metric("No. of NSE Only Companies", str(nse_only.count()[3]))
+    a4.metric("No. of BSE/NSE Companies", str(nse_bse_small_cap[0]))
 
-    base_df = (small_cap.Price >= max_stock_price)
-    st.write("Base DF", small_cap[base_df], small_cap[base_df].count())
+    min_stock_price = st.number_input("Min. Stock Price", value=25, min_value=0, max_value=100)
+    base_df = ((small_cap.Price <= max_stock_price) & (small_cap.Price >= min_stock_price))
+    stock_search = small_cap[base_df]
+    stocks_known = st.multiselect("Stocks known in Portfolio", stock_search.Name)
+
+    # Stock Search Base Dataframe
+    st.write()
+    updated_price = []
+    for i in stock_search.Price:
+        if i > avg_investment_value:
+            updated_price.append(i)
+        elif i < avg_investment_value:
+            updated_price.append((round(avg_investment_value)/round(i)) * i)
+    
+    stock_search = stock_search.assign(updated_price = updated_price)
+    stock_search = stock_search.assign(no_of_shares = round(stock_search.updated_price/stock_search.Price))
+    updated_no_of_stocks = no_of_stocks - len(stocks_known)
+
+    st.write(stock_search)
+    st.write("Base DF to iterate", small_cap[base_df].Price.count(), "Stocks for", updated_no_of_stocks, "more in Portfolio")
+    st.subheader("Stock Search")
+
+
+    ans = find_combos(stock_search.updated_price, updated_no_of_stocks)
+    # print all combinations stored in ans
+    st.write(ans)
+
+
 
 if __name__ == "__main__":
     main()
